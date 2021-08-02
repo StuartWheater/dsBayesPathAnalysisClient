708c861f016aee185f88126f724f4e9bea92e1c0
Stuart Wheater <stuart.wheater@arjuna.com>
2021-08-02 18:19:43 +0100

    Added analysis arguments 'blockList', 'varType' and 'SEMGraph'

--- R/ds.hessSEM.R
+++ R/ds.hessSEM.R
@@ -3,8 +3,11 @@
 #' @description Concatenates objects into one vector.
 #' @details To avoid combining the character names and not the 
 #' 
-#' @param resourcename a vector of character string providing the names of the objects to be combined.
-#' @param infile is the ...
+#' @param resource.name a vector of character string providing the names of the objects to be combined.
+#' @param inFile is the ...
+#' @param blockList.name is the name of ...
+#' @param varType.name is the name of...
+#' @param SEMGraph.name is the name of ...
 #' @param autoAddIntercept is the ...
 #' @param gammaInit is the ...
 #' @param nIter is the ...
@@ -15,22 +18,51 @@
 #' @param datasources a list of \code{\link{DSConnection-class}} 
 #' objects obtained after login. If the \code{datasources} argument is not specified
 #' the default set of connections will be used: see \code{\link{datashield.connections_default}}.
-#' @return  \code{ds.c} returns the vector of concatenating R
+#' @return a list from each server/study containing the results of that server/study. Each results in the
+#' list is named with the name allocated to the server/study during the login process. The results from
+#' each server/study is a list containing the following named items:
+#' HESS_beta_1: 
+#' HESS_beta_2_MCMC: 
+#' HESS_beta_2: 
+#' HESS_gamma_1_MCMC: 
+#' HESS_gamma_1: 
+#' HESS_gamma_2_MCMC: 
+#' HESS_gamma_2: 
+#' HESS_logP: 
+#' HESS_R2_complete_cases_1_MCMC: (if input contains NAs) 
+#' HESS_R2_complete_cases_2_MCMC: (if input contains NAs) 
+#' HESS_R2_full_data_1_MCMC: 
+#' HESS_R2_full_data_2_MCMC: 
 #' @author DataSHIELD Development Team
 #' @import DSI
 #' @export
-ds.hessSEM <- function(resourcename, infile, autoAddIntercept, gammaInit, nIter, burnin, nChains, seed, method, datasources=NULL){
+ds.hessSEM <- function(resource.name, inFile, blockList.name, varType.name, SEMGraph.name, autoAddIntercept, gammaInit, nIter, burnin, nChains, seed, method, datasources=NULL){
     # look for DS connections
     if(is.null(datasources)){
         datasources <- DSI::datashield.connections_find()
     }
 
-    if(is.null(resourcename)){
-        stop("Please provide the names of the resources to concatenate!", call.=FALSE)
+    if(is.null(resource.name) || (! is.character(resource.name)) || (length(resource.name) != 1)){
+        stop("Please provide the name of the resources to be used", call.=FALSE)
     }
 
-    resourcename.transfer     <- resourcename
-    infile.transfer           <- infile
+    if (is.null(blockList.name) || (! is.character(blockList.name)) || (length(blockList.name) != 1)){
+        stop("Please provide the name of the block list to be used", call.=FALSE)
+    }
+
+    if (is.null(varType.name) || (! is.character(varType.name)) || (length(varType.name) != 1)){
+        stop("Please provide the name of the var type to be used", call.=FALSE)
+    }
+
+    if (is.null(SEMGraph.name) || (! is.character(SEMGraph.name)) || (length(SEMGraph.name) != 1)){
+        stop("Please provide the name of the SEM graph to be used", call.=FALSE)
+    }
+
+    resource.name.transfer    <- resource.name
+    inFile.transfer           <- inFile
+    blockList.name.transfer   <- blockList.name
+    varType.name.transfer     <- varType.name
+    SEMGraph.name.transfer    <- SEMGraph.name
     autoAddIntercept.transfer <- autoAddIntercept
     gammaInit.transfer        <- gammaInit
     nIter.transfer            <- nIter
@@ -40,6 +72,6 @@ ds.hessSEM <- function(resourcename, infile, autoAddIntercept, gammaInit, nIter,
     method.transfer           <- method
 
     # call the server side function that does the job
-    calltext <- call("hessSEMDS", as.symbol(resourcename.transfer), infile.transfer, autoAddIntercept.transfer, gammaInit.transfer, nIter.transfer, burnin.transfer, nChains.transfer, seed.transfer, method.transfer)
+    calltext <- call("hessSEMDS", as.symbol(resource.name.transfer), inFile.transfer, as.symbol(blockList.name.transfer), as.symbol(varType.name.transfer), as.symbol(SEMGraph.name.transfer), autoAddIntercept.transfer, gammaInit.transfer, nIter.transfer, burnin.transfer, nChains.transfer, seed.transfer, method.transfer)
     DSI::datashield.aggregate(datasources, calltext)
 }
--- tests/testthat/test-smk-ds.hessSEM.R
+++ tests/testthat/test-smk-ds.hessSEM.R
@@ -19,7 +19,51 @@ connect.sem.resource.sem(ds.test.env)
 
 context("ds.hessSEM::smk::no na")
 test_that("no na hessSEM", {
-    res <- ds.hessSEM("D", "sem_data.txt", TRUE, "S", 200, 0, 1, 0, 1)
+    # list(c(9:28), c(1:5), c(6:8))
+    ds.assign(toAssign = "c(9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28)", newobj = "blockList_1")
+    ds.assign(toAssign = "c(1,2,3,4,5)", newobj = "blockList_2")
+    ds.assign(toAssign = "c(6,7,8)", newobj = "blockList_3")
+    ds.list(c("blockList_1", "blockList_2", "blockList_3"), newobj = "blockList")
+
+    # NULL
+    ds.assign(toAssign="NULL", newobj = "varType")
+
+    # matrix(c(0, 1, 1, 0, 0, 1, 0, 0, 0), byrow = TRUE, ncol = 3, nrow = 3)
+    ds.matrix(mdata = c(0,1,1,0,0,1,0,0,0), nrows.scalar = 3, ncols.scalar = 3, newobj = "SEMGraph")
+
+    res <- ds.hessSEM("D", "sem_data.txt", "blockList", "varType", "SEMGraph", TRUE, "S", 200, 0, 1, 0, 1)
+
+    expect_length(res, 1)
+    expect_length(res$sem1, 11)
+
+    expect_true(all(c("spec_tbl_df", "tbl_df", "tbl", "data.frame") %in% class(res$sem1$HESS_beta_1_MCMC)))
+    expect_true(all(c("spec_tbl_df", "tbl_df", "tbl", "data.frame") %in% class(res$sem1$HESS_beta_1)))
+    expect_true(all(c("spec_tbl_df", "tbl_df", "tbl", "data.frame") %in% class(res$sem1$HESS_beta_2_MCMC)))
+    expect_true(all(c("spec_tbl_df", "tbl_df", "tbl", "data.frame") %in% class(res$sem1$HESS_beta_2)))
+    expect_true(all(c("spec_tbl_df", "tbl_df", "tbl", "data.frame") %in% class(res$sem1$HESS_gamma_1_MCMC)))
+    expect_true(all(c("spec_tbl_df", "tbl_df", "tbl", "data.frame") %in% class(res$sem1$HESS_gamma_1)))
+    expect_true(all(c("spec_tbl_df", "tbl_df", "tbl", "data.frame") %in% class(res$sem1$HESS_gamma_2_MCMC)))
+    expect_true(all(c("spec_tbl_df", "tbl_df", "tbl", "data.frame") %in% class(res$sem1$HESS_gamma_2)))
+    expect_true(all(c("spec_tbl_df", "tbl_df", "tbl", "data.frame") %in% class(res$sem1$HESS_logP)))
+    expect_true(all(c("spec_tbl_df", "tbl_df", "tbl", "data.frame") %in% class(res$sem1$HESS_R2_full_data_1_MCMC)))
+    expect_true(all(c("spec_tbl_df", "tbl_df", "tbl", "data.frame") %in% class(res$sem1$HESS_R2_full_data_2_MCMC)))
+})
+
+context("ds.hessSEM::smk::no na")
+test_that("no na hessSEM", {
+    # list(c(9:28), c(1:5), c(6:8))
+    ds.assign(toAssign = "c(9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28)", newobj = "blockList_1")
+    ds.assign(toAssign = "c(1,2,3,4,5)", newobj = "blockList_2")
+    ds.assign(toAssign = "c(6,7,8)", newobj = "blockList_3")
+    ds.list(c("blockList_1", "blockList_2", "blockList_3"), newobj = "blockList")
+
+    # NULL
+    ds.assign(toAssign="NULL", newobj = "varType")
+
+    # matrix(c(0, 1, 1, 0, 0, 1, 0, 0, 0), byrow = TRUE, ncol = 3, nrow = 3)
+    ds.matrix(mdata = c(0,1,1,0,0,1,0,0,0), nrows.scalar = 3, ncols.scalar = 3, newobj = "SEMGraph")
+
+    res <- ds.hessSEM("D", "sem_data.txt", "blockList", "varType", "SEMGraph", TRUE, "S", 200, 0, 1, 0, 1)
 
     expect_length(res, 1)
     expect_length(res$sem1, 11)
@@ -39,7 +83,53 @@ test_that("no na hessSEM", {
 
 context("ds.hessSEM::smk::na")
 test_that("na hessSEM", {
-    res <- ds.hessSEM("D", "na_sem_data.txt", TRUE, "S", 200, 0, 1, 0, 1)
+    # list(c(9:28), c(1:5), c(6:8))
+    ds.assign(toAssign = "c(9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28)", newobj = "blockList_1")
+    ds.assign(toAssign = "c(1,2,3,4,5)", newobj = "blockList_2")
+    ds.assign(toAssign = "c(6,7,8)", newobj = "blockList_3")
+    ds.list(c("blockList_1", "blockList_2", "blockList_3"), newobj = "blockList")
+
+    # NULL
+    ds.assign(toAssign="NULL", newobj = "varType")
+
+    # matrix(c(0, 1, 1, 0, 0, 1, 0, 0, 0), byrow = TRUE, ncol = 3, nrow = 3)
+    ds.matrix(mdata = c(0,1,1,0,0,1,0,0,0), nrows.scalar = 3, ncols.scalar = 3, newobj = "SEMGraph")
+
+    res <- ds.hessSEM("D", "na_sem_data.txt", "blockList", "varType", "SEMGraph", TRUE, "S", 200, 0, 1, 0, 1)
+
+    expect_length(res, 1)
+    expect_length(res$sem1, 13)
+
+    expect_true(all(c("spec_tbl_df", "tbl_df", "tbl", "data.frame") %in% class(res$sem1$HESS_beta_1_MCMC)))
+    expect_true(all(c("spec_tbl_df", "tbl_df", "tbl", "data.frame") %in% class(res$sem1$HESS_beta_1)))
+    expect_true(all(c("spec_tbl_df", "tbl_df", "tbl", "data.frame") %in% class(res$sem1$HESS_beta_2_MCMC)))
+    expect_true(all(c("spec_tbl_df", "tbl_df", "tbl", "data.frame") %in% class(res$sem1$HESS_beta_2)))
+    expect_true(all(c("spec_tbl_df", "tbl_df", "tbl", "data.frame") %in% class(res$sem1$HESS_gamma_1_MCMC)))
+    expect_true(all(c("spec_tbl_df", "tbl_df", "tbl", "data.frame") %in% class(res$sem1$HESS_gamma_1)))
+    expect_true(all(c("spec_tbl_df", "tbl_df", "tbl", "data.frame") %in% class(res$sem1$HESS_gamma_2_MCMC)))
+    expect_true(all(c("spec_tbl_df", "tbl_df", "tbl", "data.frame") %in% class(res$sem1$HESS_gamma_2)))
+    expect_true(all(c("spec_tbl_df", "tbl_df", "tbl", "data.frame") %in% class(res$sem1$HESS_logP)))
+    expect_true(all(c("spec_tbl_df", "tbl_df", "tbl", "data.frame") %in% class(res$sem1$HESS_R2_complete_cases_1_MCMC)))
+    expect_true(all(c("spec_tbl_df", "tbl_df", "tbl", "data.frame") %in% class(res$sem1$HESS_R2_complete_cases_2_MCMC)))
+    expect_true(all(c("spec_tbl_df", "tbl_df", "tbl", "data.frame") %in% class(res$sem1$HESS_R2_full_data_1_MCMC)))
+    expect_true(all(c("spec_tbl_df", "tbl_df", "tbl", "data.frame") %in% class(res$sem1$HESS_R2_full_data_2_MCMC)))
+})
+
+context("ds.hessSEM::smk::na")
+test_that("na hessSEM", {
+    # list(c(9:28), c(1:5), c(6:8))
+    ds.assign(toAssign = "c(9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28)", newobj = "blockList_1")
+    ds.assign(toAssign = "c(1,2,3,4,5)", newobj = "blockList_2")
+    ds.assign(toAssign = "c(6,7,8)", newobj = "blockList_3")
+    ds.list(c("blockList_1", "blockList_2", "blockList_3"), newobj = "blockList")
+
+    # NULL
+    ds.assign(toAssign="NULL", newobj = "varType")
+
+    # matrix(c(0, 1, 1, 0, 0, 1, 0, 0, 0), byrow = TRUE, ncol = 3, nrow = 3)
+    ds.matrix(mdata = c(0,1,1,0,0,1,0,0,0), nrows.scalar = 3, ncols.scalar = 3, newobj = "SEMGraph")
+
+    res <- ds.hessSEM("D", "na_sem_data.txt", "blockList", "varType", "SEMGraph", TRUE, "S", 200, 0, 1, 0, 1)
 
     expect_length(res, 1)
     expect_length(res$sem1, 13)
